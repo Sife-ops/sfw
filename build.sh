078438924c2765286bdce5104e5536e29d66c9b2
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2024-02-04 09:13:12 -0800

    wip

--- build.sh
+++ build.sh
@@ -7,14 +7,4 @@ make # todo fix cubiomes makefile
 make g0 && cp a.out ../bin/cubiomes
 popd
 
-go build -o ./bin/generator \
-	./cmd/generator/main.go
-
-go build -o ./bin/scheduler \
-	./cmd/scheduler/main.go
-
-go build -o ./bin/cubiomes-worker \
-	./cmd/cubiomes-worker/main.go
-
-go build -o ./bin/worldgen-worker \
-	./cmd/worldgen-worker/main.go
+make all
