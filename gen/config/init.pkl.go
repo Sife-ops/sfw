9963b3810e85e8c15557f6ecedb34f8825ecb41b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2024-02-12 02:27:36 -0800

    wip

--- gen/config/init.pkl.go
+++ gen/config/init.pkl.go
@@ -6,7 +6,7 @@ import "github.com/apple/pkl-go/pkl"
 func init() {
 	pkl.RegisterMapping("sfw", Sfw{})
 	pkl.RegisterMapping("sfw#Postgres", PostgresImpl{})
-	pkl.RegisterMapping("sfw#Log", LogImpl{})
 	pkl.RegisterMapping("sfw#Web", WebImpl{})
+	pkl.RegisterMapping("sfw#Log", LogImpl{})
 	pkl.RegisterMapping("sfw#Websocket", WebsocketImpl{})
 }
